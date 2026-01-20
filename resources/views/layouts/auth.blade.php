8af4d7ea00346affab8a96a459cd0c3876b1dbda
zharsond <muhammad.sondjaja@satu.ac.id>
2026-01-20 08:17:08 +0700

    Menambahkan Tailwind CDN di auth.blade.php

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zeniro - @yield('title')</title>
+    <script src="https://cdn.tailwindcss.com"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     @if (session('success'))
